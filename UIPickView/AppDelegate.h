94ed783cac6bc2b9c4a1c169d57101c0f72c92d4
Dacilia <1257249648@qq.com>
2018-04-17 17:15:39 +0800

    Initial Commit

---
+++ UIPickView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIPickView
+//
+//  Created by 李达志 on 2018/4/17.
+//  Copyright © 2018年 LDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIPickView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIPickView
+//
+//  Created by 李达志 on 2018/4/17.
+//  Copyright © 2018年 LDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
